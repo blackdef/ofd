583d28d42f3e772fd83ce5f1ef49e2cb3e51a19b
blackdef <blackdef@bk.ru>
2016-10-11 09:51:07 +0300

    Настроил новый проект и подключил к нему нужные библиотеки

---
+++ tcp/src/main/java/ru/astralnalog/jmeter/OfdTCPClient.java
@@ -0,0 +1,8 @@
+package ru.astralnalog.jmeter;
+
+import org.apache.jmeter.protocol.tcp.sampler.BinaryTCPClientImpl;
+/**
+ * Created by plotnikov on 11.10.2016.
+ */
+public class OfdTCPClient extends BinaryTCPClientImpl {
+}
